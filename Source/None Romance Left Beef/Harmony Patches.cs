3028d56b5136a3a6a6b7a916aa963ecea250a0db
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2023-04-02 13:35:00 -0500

    Initial commit

---
+++ Source/None Romance Left Beef/Harmony Patches.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+using System.Reflection.Emit;
+using BetterRomance;
+using BetterRomance.HarmonyPatches;
+using UnityEngine;
+
+namespace NoneRomance
+{
+    [StaticConstructorOnStartup]
+    public static class OnStartup
+    {
+        static OnStartup()
+        {
+            Harmony harmony = new Harmony(id: "rimworld.divineDerivative.NoneRomance");
+            harmony.PatchAll();
+            if (ModsConfig.IsActive("divineDerivative.Romance") || ModsConfig.IsActive("divineDerivative.RomanceDev"))
+            {
+                harmony.PatchWBR();
+            }
+        }
+    }
+
+    [HarmonyPatch]
+    public static class CorePatches
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(SocialCardUtility), "CanDrawTryRomance")]
+        public static void CanDrawTryRomancePostfix(Pawn pawn, ref bool __result)
+        {
+            if (__result && NoneRomanceMod.settings.hideButton)
+            {
+                __result = false;
+            }
+        }
+
+        [HarmonyTranspiler]
+        [HarmonyPatch(typeof(FloatMenuMakerMap), "AddHumanlikeOrders")]
+        public static IEnumerable<CodeInstruction> AddHumanlikeOrdersTranspiler(IEnumerable<CodeInstruction> instructions)
+        {
+            MethodInfo Drafted = AccessTools.PropertyGetter(typeof(Pawn), nameof(Pawn.Drafted));
+            MethodInfo BiotechActive = AccessTools.PropertyGetter(typeof(ModsConfig), nameof(ModsConfig.BiotechActive));
+
+            object jumpLabel = new object();
+            bool labelFound = false;
+            List<CodeInstruction> codes = new List<CodeInstruction>(instructions);
+            for (int i = 0; i < codes.Count; i++)
+            {
+                yield return codes[i];
+                if (codes[i].Calls(Drafted) && codes[i + 1].opcode == OpCodes.Brtrue && codes[i + 2].Calls(BiotechActive))
+                {
+                    jumpLabel = codes[i + 1].operand;
+                    labelFound = true;
+                }
+                if (labelFound && codes[i].opcode == OpCodes.Brfalse && codes[i - 1].Calls(BiotechActive))
+                {
+                    yield return CodeInstruction.LoadField(typeof(NoneRomanceMod), nameof(NoneRomanceMod.settings));
+                    yield return CodeInstruction.LoadField(typeof(Settings), nameof(Settings.hideMenu));
+                    yield return new CodeInstruction(OpCodes.Brtrue, jumpLabel);
+                }
+            }
+        }
+    }
+
+    public static class WBRPatches
+    {
+        public static void CanDrawTryHookupPostfix(Pawn pawn, ref bool __result)
+        {
+            if (__result && NoneRomanceMod.settings.WBRHideButton)
+            {
+                __result = false;
+            }
+        }
+
+        public static bool AddHumanlikeOrdersPrefix()
+        {
+            return !NoneRomanceMod.settings.WBRHideMenu;
+        }
+
+        public static void PatchWBR(this Harmony harmony)
+        {
+            harmony.Patch(AccessTools.Method(typeof(HookupUtility), nameof(HookupUtility.CanDrawTryHookup)), postfix: new HarmonyMethod(typeof(WBRPatches), nameof(WBRPatches.CanDrawTryHookupPostfix)));
+            harmony.Patch(AccessTools.Method(typeof(FloatMenuMakerMap_AddHumanlikeOrders), nameof(FloatMenuMakerMap_AddHumanlikeOrders.Postfix)), prefix: new HarmonyMethod(typeof(WBRPatches), nameof(WBRPatches.AddHumanlikeOrdersPrefix)));
+        }
+    }
+}
---
+++ Source/None Romance Left Beef/Mod Settings.cs
@@ -0,0 +1,81 @@
+﻿using Verse;
+using UnityEngine;
+
+namespace NoneRomance
+{
+    public class Settings : ModSettings
+    {
+        public bool hideButton = true;
+        public bool hideMenu = true;
+        public bool WBRHideButton = true;
+        public bool WBRHideMenu = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref hideButton, "hideRomanceButton", true);
+            Scribe_Values.Look(ref hideMenu, "hideRomanceMenuOption", true);
+            Scribe_Values.Look(ref WBRHideButton, "WBRHideButton", true);
+            Scribe_Values.Look(ref WBRHideMenu, "WBRHideMenu", true);
+            base.ExposeData();
+        }
+    }
+
+    public class NoneRomanceMod : Mod
+    {
+        public static Settings settings;
+
+        public NoneRomanceMod(ModContentPack content) : base(content)
+        {
+            settings = GetSettings<Settings>();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "NRLB.ModTitle".Translate();
+        }
+
+        public override void DoSettingsWindowContents(Rect canvas)
+        {
+            Listing_Standard list = new Listing_Standard
+            {
+                ColumnWidth = (canvas.width / 2f) - 17f
+            };
+            list.Begin(canvas);
+            if (ModsConfig.BiotechActive)
+            {
+                list.Label("NRLB.RomanceHeader".Translate());
+                CheckboxLabledTabAndTooltip(list, "NRLB.RomanceButton".Translate(), ref settings.hideButton, 25f, "NRLB.RomanceButtonTooltip".Translate());
+                CheckboxLabledTabAndTooltip(list, "NRLB.RomanceMenu".Translate(), ref settings.hideMenu, 25f, "NRLB.RomanceMenuTooltip".Translate());
+                list.GapLine();
+            }
+            if (ModsConfig.IsActive("divineDerivative.Romance") || ModsConfig.IsActive("divineDerivative.RomanceDev"))
+            {
+                list.Label("NRLB.HookupHeader".Translate());
+                CheckboxLabledTabAndTooltip(list, "NRLB.HookupButton".Translate(), ref settings.WBRHideButton, 25f, "NRLB.HookupButtonTooltip".Translate());
+                CheckboxLabledTabAndTooltip(list, "NRLB.HookupMenu".Translate(), ref settings.WBRHideMenu, 25f, "NRLB.HookupMenuTooltip".Translate());
+            }
+            list.End();
+            base.DoSettingsWindowContents(canvas);
+        }
+
+        private void CheckboxLabledTabAndTooltip(Listing_Standard list, string label, ref bool checkOn, float tabIn, string tooltip)
+        {
+            float height = Text.CalcHeight(label, list.ColumnWidth);
+            Rect rect = list.GetRect(height);
+            rect.xMin += tabIn;
+            if (!list.BoundingRectCached.HasValue || rect.Overlaps(list.BoundingRectCached.Value))
+            {
+                if (!tooltip.NullOrEmpty())
+                {
+                    if (Mouse.IsOver(rect))
+                    {
+                        Widgets.DrawHighlight(rect);
+                    }
+                    TooltipHandler.TipRegion(rect, tooltip);
+                }
+                Widgets.CheckboxLabeled(rect, label, ref checkOn);
+            }
+            list.Gap(list.verticalSpacing);
+        }
+    }
+}
