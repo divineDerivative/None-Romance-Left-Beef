adfd8ce1fcb00ac7c11ed7f5cc9913f045017b0e
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-07-14 10:26:06 -0500

    DivineFramework 0.9.0.24

--- Source/Mod Settings.cs
+++ Source/Mod Settings.cs
@@ -75,7 +75,7 @@ namespace NoneRomance
         public NoneRomanceMod(ModContentPack content) : base(content)
         {
             settings = GetSettings<Settings>();
-            ModManagement.RegisterMod("NRLB.ModTitle", typeof(NoneRomanceMod).Assembly.GetName().Name, new("0.9.0.8"), "[NoneRomance]", () => true);
+            ModManagement.RegisterMod("NRLB.ModTitle", typeof(NoneRomanceMod).Assembly.GetName().Name, new(FrameworkVersionInfo.Version), "[NoneRomance]", () => true);
         }
 
         public override string SettingsCategory()
---
+++ Source/Properties/FrameworkVersion.g.cs
@@ -0,0 +1,7 @@
+namespace NoneRomance
+{
+    internal static class FrameworkVersionInfo
+    {
+        public const string Version = "0.9.0.24";
+    }
+}
