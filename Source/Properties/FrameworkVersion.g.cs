e9635ca4cb8b329bc2e3b5fb8d8ba5d30a9ed0cc
divineDerivative <78392890+divineDerivative@users.noreply.github.com>
2025-07-17 13:19:39 -0500

    DivineFramework 0.11.0.2

--- Source/Properties/FrameworkVersion.g.cs
+++ Source/Properties/FrameworkVersion.g.cs
@@ -2,6 +2,6 @@ namespace NoneRomance
 {
     internal static class FrameworkVersionInfo
     {
-        public const string Version = "0.11.0.1";
+        public const string Version = "0.11.0.2";
     }
 }
